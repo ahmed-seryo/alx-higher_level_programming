4aafdb4a2abe724488e0fabe8615ea0a0e0c000d
aswany <asaid.megaci@yahoo.com>
2024-03-20 22:09:46 +0000

    new

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        max_score = max(list(a_dictionary.values()))
-        for k in a_dictionary.keys():
-            if a_dictionary[k] == max_score:
-                return k
-                                                                else:
-                                                                    return None
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
