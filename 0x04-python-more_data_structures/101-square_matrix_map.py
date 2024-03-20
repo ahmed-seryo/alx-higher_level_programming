862a4bb2a808814f3e80396948c3454d7130a372
aswany <asaid.megaci@yahoo.com>
2024-03-20 22:16:41 +0000

    101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda l: (list(map(lambda x: x**2, l))), matrix))
+    return list(map(lambda 1: (list(map(lambda x: x**2, 1))), matrix))
